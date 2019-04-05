d6e9ee68981234c1a395b3e4f3530c910432f3ce
Peter Israel <46018804+Peterisraelo@users.noreply.github.com>
2019-04-05 23:46:44 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ function shuffle(array) {
 
 document.body.onload =  function(){
     cards = shuffle(cards);
-    
+
     decker.innerHTML = "";
 
     cards.forEach(function(item){
@@ -36,10 +36,23 @@ document.body.onload =  function(){
 
 cards.forEach(function(car){
     car.addEventListener("click", function(){
-        car.className+= " open show"
+        car.className+= " open show disabled"
         openCards.push(this);
+        if((openCards.length)==2){
+            if((openCards[0].querySelector("i").className)==(openCards[1].querySelector("i").className)){
+                (openCards[0].className+= " match");
+                (openCards[1].className+= " match");
+                openCards = [];
+            }else{
+                openCards[0].className+= " wrong";
+                openCards[1].className+= " wrong";
+                setTimeout(function(){
+                    (openCards[0].classList.remove("open","show","disabled","wrong"));
+                    (openCards[1].classList.remove("open","show","disabled","wrong"));
+                    openCards = [];
+                }, 1100);
+            }
+        }
     });
 });
-
-
 
